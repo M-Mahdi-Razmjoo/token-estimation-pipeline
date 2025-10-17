844efd6ed52c420ac84327d8e03aa6ab90fbf164
M-Mahdi-Razmjoo <m.mahdi.razmjoo@gmail.com>
2025-10-17 22:47:40 +0330

    update log file

--- src/utils.py
+++ src/utils.py
@@ -1,29 +1,25 @@
 import sys
-import pandas
-import numpy
-import sklearn
-import pyarrow
-import tqdm
-import regex
+import platform
+from importlib import metadata
 
 def get_environment_info():
-    info_str = "environment & library versions\n"
-    info_str += "-"*60
-
-    py_version = f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
-    info_str += f"{'Python Version:':<20} {py_version}\n"
-
-    libraries = {
-        "scikit-learn": sklearn,
-        "pandas": pandas,
-        "numpy": numpy,
-        "pyarrow": pyarrow,
-        "tqdm": tqdm,
-        "regex": regex
-    }
-    
-    for name, lib in libraries.items():
-        info_str += f"{name.capitalize() + ' Version:':<20} {lib.__version__}\n"
-            
-    info_str += "-"*60
+    info_str = "Environment, Python & Library Versions\n"
+    info_str +="-"*60+"\n"
+    info_str += f"{'OS Platform:':<25} {platform.platform()}\n"
+    py_version = sys.version.replace('\n', ' ')
+    py_arch = platform.architecture()[0]
+    info_str += f"{'Python Version:':<25} {py_version}\n"
+    info_str += f"{'Python Architecture:':<25} {py_arch}\n"
+    info_str += "Installed Packages & Versions in Environment:\n"
+    info_str +="-"*60+"\n"
+    installed_packages = sorted([f"{dist.metadata['Name']} ({dist.version})" for dist in metadata.distributions()],key=str.lower)
+    col_width = max(len(p) for p in installed_packages) + 4
+    num_cols = max(1, 100 // col_width)
+    rows = []
+    for i in range(0, len(installed_packages), num_cols):
+        row_items = installed_packages[i:i+num_cols]
+        padded_items = [item.ljust(col_width) for item in row_items]
+        rows.append("".join(padded_items))
+    info_str +="\n".join(rows)
+    info_str +="-"*60+"\n\n"
     return info_str
